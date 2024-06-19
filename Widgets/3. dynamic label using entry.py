47ec84222bfa7eacdf47ed1c407570e11681a84a
TKNA <Brodyjeanes1@gmail.com>
2024-06-19 15:38:01 +0800

    Finished Dynamic Labels

--- Widgets/3. dynamic label using entry.py
+++ Widgets/3. dynamic label using entry.py
@@ -5,6 +5,9 @@ def buttonFunction():
     # Get the label string
     print(labelWidgetString_Var.get())
 
+    # Set the label and entry widget string 
+    labelWidgetString_Var.set("BUTTON PRESSED")
+
 
 # Create window
 window = tk.Tk()
@@ -14,6 +17,8 @@ window.title("Dynamic Label Updating")
 # tkinter dynamic variable
 labelWidgetString_Var = tk.StringVar()
 # labelWidgetString_Var = tk.StringVar(value = "STARTING VALUE")
+NEWLabelWidgetString_Var = tk.StringVar(value = "START")
+
 
 
 # Create widgets
@@ -28,9 +33,28 @@ entryWidget = ttk.Entry(
     textvariable = labelWidgetString_Var)
 entryWidget.pack()
 
-buttonWidget = ttk.Button(master = window, text = "Button", command = buttonFunction)
-buttonWidget.pack()
+buttonWidget = ttk.Button(
+    master = window, 
+    text = "Button", 
+    command = buttonFunction)
+buttonWidget.pack(pady=30)
 
+# HELPFUL EXERCISE
+NEWLabelWidget = ttk.Label(
+    master = window, 
+    text = "PLACEHOLDER",
+    textvariable = NEWLabelWidgetString_Var)
+NEWLabelWidget.pack()
+
+NEWEntryWidget = ttk.Entry(
+    master = window, 
+    textvariable = NEWLabelWidgetString_Var )
+NEWEntryWidget.pack()
+
+OTHEREntryWidget = ttk.Entry(
+    master = window, 
+    textvariable = NEWLabelWidgetString_Var)
+OTHEREntryWidget.pack()
 
 # Run window
 window.mainloop()
