366999a8c6b0eb087fce3201034d48adbb26eff1
TKNA <Brodyjeanes1@gmail.com>
2024-07-04 18:52:45 +0800

    Create 6. widget sizes.py

---
+++ Layouts/6. widget sizes.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Create window
+window = tk.Tk()
+window.title("Basic Layouts")
+window.geometry("600x400")
+
+
+
+
+
+
+# Run window
+window.mainloop()
